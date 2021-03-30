759cc6cb70932be8a877b427d98ac49b6966dcd7
Laura Messa Travin <laura.travin@gmail.com>
2021-03-30 22:14:02 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,30 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
-  
+
   def take_ride
+    if take_ride_ok?
+      update_user
+    end
+  end
+  def take_ride_ok?
+     if self.user.tickets >= self.attraction.tickets && self.user.height >= self.attraction.min_height
+      true
+     else
+       false
+     end
+
+  end
+
+  def update_user
+    new_happiness = self.user.happiness + self.attraction.happiness_rating
+    new_nausea = self.user.nausea + self.attraction.nausea_rating
+    new_ticket_count = self.user.tickets - self.attraction.tickets
+    self.user.update(
+      :happiness => new_happiness,
+      :nausea => new_nausea,
+      :tickets => new_ticket_count
+    )
+    "Thanks for riding the #{self.attraction.name}!"
   end
 end
